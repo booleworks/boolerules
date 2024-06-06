4861ebbb73716853c4cacfd8a335c51e96ea14de
Christoph Zengler <christoph.zengler@booleworks.com>
2024-06-06 21:50:39 +0200

    SHOWCASE: new structure for sidebar

--- showcase/components/MenuSidebar.vue
+++ showcase/components/MenuSidebar.vue
@@ -2,71 +2,67 @@
     <div class="border-right-1 surface-border overflow-scroll">
         <div class="m-3">
             <a href="/">
-                <img :src="currentLogo()" alt="Logo" width="200" />
+                <img :src="currentLogo()" alt="Logo" width="210" />
             </a>
         </div>
-        <div class="menu-header"><i class="pi pi pi-fw pi-file"></i> {{ $t('computation.header_rulefile').toUpperCase()
+        <div class="menu-header"><i class="pi pi-fw pi-eye"></i>{{ ' ' + $t('computation.header_insights').toUpperCase()
             }}
         </div>
-        <MenuItem v-for="algo in rulefileAlgos" :item="algo" />
-        <div class="menu-header"><i class="pi pi pi-fw pi-box"></i>{{
-            $t('computation.header_configuration').toUpperCase()
+        <MenuItem v-for="algo in insightAlgos" :item="algo" />
+        <div class="menu-header"><i class="pi pi-fw pi-sliders-h"></i>{{
+            ' ' + $t('computation.header_configuration').toUpperCase()
             }}
         </div>
         <MenuItem v-for="algo in configurationAlgos" :item="algo" />
-        <div class="menu-header"><i class="pi pi pi-fw pi-sort-amount-down"></i>{{
-            $t('computation.header_optimization').toUpperCase() }}
+        <div class="menu-header"><i class="pi pi-fw pi-sort-amount-down"></i>{{
+            ' ' + $t('computation.header_optimization').toUpperCase() }}
         </div>
         <MenuItem v-for="algo in optimizationAlgos" :item="algo" />
-        <!-- <div class="menu-header"><i class="pi pi pi-fw pi-sitemap"></i>{{ $t('computation.header_bom').toUpperCase() }}</div> -->
-        <!-- <MenuItem v-for="algo in bomAlgos" :item="algo" /> -->
-        <div class="menu-header"><i class="pi pi pi-fw pi-desktop"></i>{{
-            $t('computation.header_software').toUpperCase() }}
+        <div class="menu-header"><i class="pi pi-fw pi-sitemap"></i>{{ ' ' + $t('computation.header_bom').toUpperCase()
+            }}
+        </div>
+        <MenuItem v-for="algo in bomAlgos" :item="algo" />
+        <div class="menu-header"><i class="pi pi-fw pi-box"></i>{{
+            ' ' + $t('computation.header_software').toUpperCase() }}
         </div>
         <MenuItem v-for="algo in softwareAlgos" :item="algo" />
-        <!-- <div class="menu-header"><i class="pi pi pi-fw pi-share-alt"></i>{{ $t('computation.header_vis').toUpperCase() }} -->
-        <!-- </div> -->
-         <MenuItem v-for="algo in visualizationAlgos" :item="algo" />
     </div>
 </template>
 
 <script setup lang="ts">
-import {useI18n} from 'vue-i18n'
+import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 const { currentLogo } = useTheme()
 
-const rulefileAlgos = ref([
-    { label: t('computation.consistency'), to: '/computation/rulefile/consistency' },
-    { label: t('computation.backbone'), to: '/computation/rulefile/backbone' },
-    { label: t('computation.enumeration'), to: '/computation/rulefile/enumeration' },
+const insightAlgos = ref([
     { label: t('computation.counting'), to: '/computation/rulefile/counting' },
-])
-const configurationAlgos = ref([
-    { label: t('computation.minmax'), to: '/computation/configuration/minmax' },
-    { label: t('computation.reconfiguration'), to: '/computation/configuration/reconfiguration' },
-    // { label: t('computation.constraints'), to: '' },
-    // { label: t('computation.bulk'), to: '' },
-])
-const optimizationAlgos = ref([
-    { label: t('computation.weights'), to: '/computation/optimization/optimization' },
-    { label: t('computation.coverage'), to: '/computation/optimization/coverage' },
-])
-// const bomAlgos = ref([
-//   { label: t('computation.pos_uniqueness'), to: '' },
-//   { label: t('computation.pos_dead'), to: '' },
-// ])
-const softwareAlgos = ref([
-    //   { label: t('computation.pkg_buildability'), to: '' },
-    { label: t('computation.pkg_solving'), to: '/computation/software/packagesolving' },
-    //   { label: t('computation.pkg_optimization'), to: '' },
-])
-const visualizationAlgos = ref([
-  { label: t('computation.constraint_graph'), to: '/computation/visualization/constraintgraph' },
-  // { label: t('computation.feature_cloud'), to: '' },
+    { label: t('computation.constraint_graph'), to: '/computation/visualization/constraintgraph' },
 ])
 
+const configurationAlgos = ref([
+    { label: t('computation.consistency'), to: '/computation/rulefile/consistency' },
+    { label: t('computation.reconfiguration'), to: '/computation/configuration/reconfiguration' },
+    { label: t('computation.backbone'), to: '/computation/rulefile/backbone' },
+    { label: t('computation.enumeration'), to: '/computation/rulefile/enumeration' },
+])
+
+const optimizationAlgos = ref([
+    { label: t('computation.minmax'), to: '/computation/configuration/minmax' },
+    { label: t('computation.coverage'), to: '/computation/optimization/coverage' },
+    { label: t('computation.weights'), to: '/computation/optimization/optimization' },
+])
+
+const bomAlgos = ref([
+    { label: t('computation.pos_validity'), to: '' },
+])
+
+const softwareAlgos = ref([
+    { label: t('computation.packagesolving'), to: '/computation/software/packagesolving' },
+])
 </script>
+
+
 
 <style scoped>
 .menu-header {
