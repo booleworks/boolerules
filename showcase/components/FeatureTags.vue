d220eff22c28562806c80fb5c596df3fc8f1a8d1
Christoph Zengler <christoph.zengler@booleworks.com>
2024-06-09 09:07:47 +0200

    SHOWCASE: force feature tag colors

--- showcase/components/FeatureTags.vue
+++ showcase/components/FeatureTags.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <Tag v-if="boolFeature" class="feature-tag ml-2 bg-[#004f93]" value="bool" />
-        <Tag v-if="enumFeature" class="feature-tag ml-2 bg-[#006266]" value="enum" />
-        <Tag v-if="intFeature" class="feature-tag ml-2 bg-[#ffc612]" value="int" />
+        <Tag v-if="boolFeature" class="feature-tag ml-2 !bg-[#004f93]" value="bool" />
+        <Tag v-if="enumFeature" class="feature-tag ml-2 !bg-[#006266]" value="enum" />
+        <Tag v-if="intFeature" class="feature-tag ml-2 !bg-[#ffc612]" value="int" />
     </div>
 </template>
 
